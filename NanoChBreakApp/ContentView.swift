c686c5f1cc5cf4e9c0b608deacb672ddeff862e6
Roa Mohamed <70070721+RoaPro9@users.noreply.github.com>
2023-01-08 17:54:38 +0300

    last changes splash
    last changes splash screen

--- NanoChBreakApp/ContentView.swift
+++ NanoChBreakApp/ContentView.swift
@@ -10,15 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.accentColor, Color("Color1"), Color("Color2")]), startPoint: .top, endPoint: .bottom)
+//            LinearGradient(gradient: Gradient(colors: [
+//                Color("Color 1"),
+//                Color("Color 2"),
+//                Color("Color 3"),
+//                Color("Color 5"), Color("Color 8"),
+//
+//            ]), startPoint: .top, endPoint: .bottom)
 //            Color.accentColor
 //            LinearGradient(colors: [Color("Color1"),Color("Color2")], startPoint: .top, endPoint: .bottom)
             VStack {
-                
-                Text("test, world!")
+//                Image("breakLogo").resizable()
+//                    .fixedSize()
+//                Text("BREAK")
+//                    .foregroundColor(.white)
+//                    .font(.system(size:45 ))
+                    
             }
             .padding()
-        }}
+        }.ignoresSafeArea()
+        
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
