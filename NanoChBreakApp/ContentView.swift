aca68b3b159e6c392f7131f3fbcb1960d0b6cabe
Roa Mohamed <70070721+RoaPro9@users.noreply.github.com>
2023-01-08 15:44:45 +0300

    Initial commit

---
+++ NanoChBreakApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NanoChBreakApp
+//
+//  Created by Roa Moha on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NanoChBreakApp/NanoChBreakAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChBreakAppApp.swift
+//  NanoChBreakApp
+//
+//  Created by Roa Moha on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct NanoChBreakAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
